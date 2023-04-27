dfb3832df27bf985e1b0c6d0cbf415b75d387bcd
Strucker.eth <aberamacarleton@gmail.com>
2023-04-27 19:32:55 +0300

    Page buildup

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,19 +3,20 @@ import Image from 'next/image';
 export default function Cards() {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><image src="/images/vercel.svg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      Shoes!
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <figure>
+        <Image src="/public/vercel.svg" alt="Shoes" width={500} height={500} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          Shoes!
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
+  );
 }
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,7 @@ export default function Home() {
     </div>
   </div>
 </div>
+  
    
    <div id="car_Cards" class="flex justify-center mt-3 mb-4 flex-wrap gap-7">
 <Cards />
@@ -28,6 +29,39 @@ export default function Home() {
 <Cards />
      <Cards />
      </div>
+   
+    <div class="stats shadow justify-center flex mx-5 my-5">
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    </div>
+    <div class="stat-title">Downloads</div>
+    <div class="stat-value">31K</div>
+    <div class="stat-desc">Jan 1st - Feb 1st</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+    </div>
+    <div class="stat-title">New Users</div>
+    <div class="stat-value">4,200</div>
+    <div class="stat-desc">↗︎ 400 (22%)</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+    </div>
+    <div class="stat-title">New Registers</div>
+    <div class="stat-value">1,200</div>
+    <div class="stat-desc">↘︎ 90 (14%)</div>
+  </div>
+  
+</div>
+
+
 
 <div id="CTA" >
 <div class="hero min-h-screen bg-base-200">
@@ -35,17 +69,63 @@ export default function Home() {
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">Hello there</h1>
       <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <div id="steps" class="justify-center flex mt-4 mb-4">
+
+<ul class="steps steps-vertical lg:steps-horizontal">
+  <li class="step step-primary">Register</li>
+  <li class="step step-primary">Choose plan</li>
+  <li class="step">Purchase</li>
+  <li class="step">Receive Product</li>
+</ul>
+
+
+</div>
+
       <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
 
+
 </div>
 
 
 
 
 
+
+
+
+
+
+
+<div id="footer">
+
+
+<footer class="footer p-10 bg-neutral text-neutral-content">
+  <div>
+    <span class="footer-title">Services</span> 
+    <a class="link link-hover">Branding</a>
+    <a class="link link-hover">Design</a>
+    <a class="link link-hover">Marketing</a>
+    <a class="link link-hover">Advertisement</a>
+  </div> 
+  <div>
+    <span class="footer-title">Company</span> 
+    <a class="link link-hover">About us</a>
+    <a class="link link-hover">Contact</a>
+    <a class="link link-hover">Jobs</a>
+    <a class="link link-hover">Press kit</a>
+  </div> 
+  <div>
+    <span class="footer-title">Legal</span> 
+    <a class="link link-hover">Terms of use</a>
+    <a class="link link-hover">Privacy policy</a>
+    <a class="link link-hover">Cookie policy</a>
+  </div>
+</footer>
+
+</div>
 
     </>
   )
